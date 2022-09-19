5b07a9730a79a2be92ada75eff39b6cffa707a9f
lea moesch <leamoesch@gmail.com>
2022-09-19 16:11:22 +1000

    icons + missing pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 
 
 export default function footer() {
   return (
     <div className="footer">
-      <p>Frontend Developer</p>
-      <button></button>
+      <ul>
+        <li className='__footer-icons'><FaGithubSquare color="918E9B" size="40px" /></li>
+        <li className='__footer-icons'><FaLinkedin color="918E9B" size="40px" /></li>
+      </ul>
     </div>
   )
 }
+

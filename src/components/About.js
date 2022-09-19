d4e1e2511de888bd97ff417eee958b96b8a4d01f
lea moesch <leamoesch@gmail.com>
2022-09-19 14:45:30 +1000

    new About file + style

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function about() {
+  return (
+    <div className="content">
+      <h1 className="title_tags">About</h1>
+      <p className="text_tags">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+    </div>
+  )
+}
